08a53de429ac03eaf66b2229355151716e1c6d3f
LynithDev <61880709+LynithDev@users.noreply.github.com>
2023-06-29 21:21:11 +0200

    F*ck me

--- utils/mongo.ts
+++ utils/mongo.ts
@@ -1,20 +1,11 @@
 import { MongoClient } from "mongodb";
 
-const URI = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_IP}:${process.env.MONGO_PORT}/?directConnection=true`
+const URI = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_IP}:${process.env.MONGO_PORT}/?directConnection=true`
 
 const client = new MongoClient(URI);
 let _isConnected = false;
 
 export async function connect() {
-    // const timeout = setTimeout(() => {
-    //     throw new Error('Failed to connect to database');
-    // }, 10 * 1000);
-
-    // await client.connect();
-    // _isConnected = true;
-    // console.log("Connected to MongoDB");
-    // clearTimeout(timeout);
-
     console.log("Attempting to connect to " + URI);
     client.connect().then(() => {
         _isConnected = true;
